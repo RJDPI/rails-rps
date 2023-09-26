6ff4b9cb2ce0825cf478c9959e1555a09deff7e9
Ricardo Jimenez <124076987+RJDPI@users.noreply.github.com>
2023-09-26 15:01:45 +0000

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+get("/rock", {:controller =>"zebra", :action => "giraffe" })
 
+get("/paper", {:controller =>"zebra", :action => "lion" })
+
+get("/scissors",) {:controller => "zebra", :action => "gorilla"})
+
+get("/", {:controller => "zebra", :action => "rules"})
 end
+
+
